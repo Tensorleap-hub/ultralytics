70999039c2f020ae3ef884faee82deb1fc7d7166
yamtawachi <yam.tawachi@tensorleap.ai>
2025-08-26 10:54:31 +0300

    idans mapping

--- leap_binder.py
+++ leap_binder.py
@@ -16,6 +16,13 @@ from code_loader.inner_leap_binder.leapbinder_decorators import (tensorleap_prep
 from code_loader.contract.responsedataclasses import BoundingBox
 from code_loader.contract.visualizer_classes import LeapImageWithBBox
 from code_loader.utils import rescale_min_max
+
+from code_loader.contract.mapping import leap_output
+from code_loader.default_metrics import categorical_crossentropy
+from code_loader.visualizers.default_visualizers import default_image_visualizer
+
+
+
 from ultralytics.utils.plotting import output_to_target #doable
 from ultralytics.utils.metrics import box_iou #doable
 
